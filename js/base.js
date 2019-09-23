bcdc9ceb1e54c527dcf5a72afa2c16ecc96f49c6
shota nitta <dustbox.1218@gmail.com>
2019-09-23 21:19:50 +0900

    Deployed 370df98 with MkDocs version: 1.0.4

--- js/base.js
+++ js/base.js
@@ -1,20 +1,33 @@
-
-/* Highlight */
-$( document ).ready(function() {
-    hljs.initHighlightingOnLoad();
-    $('table').addClass('table table-striped table-hover');
+/*
+$('.dropdown-submenu > a').on("click", function(e) {
+    var submenu = $(this);
+    $('.dropdown-submenu .dropdown-menu').removeClass('show');
+    submenu.next('.dropdown-menu').addClass('show');
+    e.stopPropagation();
 });
 
-
-$('body').scrollspy({
-    target: '.bs-sidebar',
+$('.dropdown').on("hidden.bs.dropdown", function() {
+    // hide any open menus when parent closes
+    $('.dropdown-menu.show').removeClass('show');
 });
+*/
 
+$( document ).ready( function () {
+    $( '.navbar a.dropdown-toggle' ).on( 'click', function ( e ) {
+        var $el = $( this );
+        var $parent = $( this ).offsetParent( ".dropdown-menu" );
+        $( this ).parent( "li" ).toggleClass( 'show' );
 
-/* Prevent disabled links from causing a page reload */
-$("li.disabled a").click(function() {
-    event.preventDefault();
-});
+        if ( !$parent.parent().hasClass( 'navbar-nav' ) ) {
+            $el.next().css( { "top": $el[0].offsetTop, "left": $parent.outerWidth() - 4 } );
+        }
+        $( '.navbar-nav li.show' ).not( $( this ).parents( "li" ) ).removeClass( "show" );
+        return false;
+    } );
 
-
-
+    $( '.contributors img[data-src]' ).each( function() {
+        src = $(this).attr("data-src");
+        $(this).attr('src',src);
+    });
+    $('.metadata').detach().insertAfter( '#content h1:first' );
+} );
